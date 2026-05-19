1e5f216726baecb6c7e06a97f3537db065a84c74
KepperCommander <isaramirez5279@gmail.com>
2026-05-19 09:30:08 -0500

    add match events, 1:1 result reation and validation logic fase5

--- SportsLeague/SportsLeague.DataAccess/obj/Debug/net9.0/SportsLeague.DataAccess.AssemblyInfo.cs
+++ SportsLeague/SportsLeague.DataAccess/obj/Debug/net9.0/SportsLeague.DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SportsLeague.DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c3b2dc3e5038fb99779d6ba2b04ec1aea100d35")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41a407d240f9b700adc65fdffeef08a2df143383")]
 [assembly: System.Reflection.AssemblyProductAttribute("SportsLeague.DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SportsLeague.DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
